f44bd594a6d2318a9b2402ee6538aec0ab65a909
SiriusCM <963937371@qq.com>
2026-03-17 01:10:17 +0800

    网站

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
 import axios from 'axios'
@@ -7,9 +7,9 @@ import './assets/styles.css'
 axios.defaults.withCredentials = true
 
 if (import.meta.env.PROD) {
-  axios.defaults.baseURL = 'http://127.0.0.1:8000/'
+    axios.defaults.baseURL = 'http://127.0.0.1:8000/'
 } else {
-  axios.defaults.baseURL = ''
+    axios.defaults.baseURL = ''
 }
 
 createApp(App).use(router).mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,16 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-const isCI = !!process.env.GITHUB_ACTIONS;
-const basePath = process.env.VITE_BASE_PATH || (isCI ? '/direct/' : './');
-
 export default defineConfig({
-  base: basePath,
-  plugins: [vue()],
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:8000',
-        changeOrigin: true
-      }
+    base: /direct/,
+    plugins: [vue()],
+    server: {
+        port: 5173,
+        proxy: {
+            '/api': {
+                target: 'http://127.0.0.1:8000',
+                changeOrigin: true
+            }
+        }
     }
-  }
 })
