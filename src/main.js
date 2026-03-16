3a7ed021ac56d56ba5f87c87c0687043b0a32b71
SiriusCM <963937371@qq.com>
2026-03-17 00:28:58 +0800

    初始化

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import axios from 'axios'
+import './assets/styles.css'
+
+axios.defaults.withCredentials = true
+
+if (import.meta.env.PROD) {
+  axios.defaults.baseURL = 'http://127.0.0.1:8000/'
+} else {
+  axios.defaults.baseURL = ''
+}
+
+createApp(App).use(router).mount('#app')
